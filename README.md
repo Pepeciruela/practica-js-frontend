2db37d98a450916b7a0fbf31040641010b6e1750
Angel Calvo <calvo.monzon.angel@gmail.com>
2021-10-17 13:27:10 +0200

    añado README

---
+++ README.md
@@ -0,0 +1,27 @@
+# Práctica Nodepop Front-end
+
+## Modo de uso:
+
+- Clonar o descargar el repositorio.
+- Descargar el repositorio sobre el que se sustenta el Backend de la aplicación:
+- > https://github.com/kasappeal/sparrest.js
+- Abrir dos terminales diferentes, uno para la carpeta del Backend y otro para este repositorio.
+- Copiar el archivo ```db.json```en el repositorio del Backend para que actue como fichero de la base de datos.
+- Inicializa el Backend
+    - ```npm i```para instalar las dependencias
+    - ```npm start```para arrancar el servidor
+- Una vez arrancado ves al terminar de la aplicación e inicia la pagina principal ```ìndex.html```
+- Una vez estés dentro de la página principal tendrás acceso a las siguientes funcionalidades:
+
+Esta práctica contine las siguientes páginas para su uso:
+
+* index.html --> Muestra la página principal con los anuncios ya establecidos.
+* singup.html --> Permite registrar un nuevo usuario para crear anuncios de compra y venta.
+* login.html --> Permite logearse a un usuario ya registrado.
+* new.html --> Permite crear nuevos anuncios. Solo es una opción válida para usuarios registrados en la web y logueados.
+* detail.html --> Muestra el detalle de un anuncio de la página de inicio. Si el usuaio es el creador de dicho anuncio le es permitido borrarlo desde la misma.
+
+### Observaciones
+* El buscador es solo una caja estática, no realiza filtros de búsqueda
+
+
--- js/views.js
+++ js/views.js
@@ -1,9 +1,9 @@
 export function adView(ad) {
     return `<a href="/detail.html?id=${ad.id}">
     <div name="post">
-        <div class="card mb-3">
+        <div class="card mb-3" id="card-header">
         <h3 class="card-header">${ad.nombre}</h3>
-        <img src="${ad.foto}" style= "max-width: 300px; max-height: 300px;"></img>
+        <img src="${ad.foto}" class ="foto"></img>
         <ul class="list-group list-group-flush">
         <li class="list-group-item">${ad.venta}</li>
         <li class="list-group-item">Precio:<span>${ad.precio}</span> €</li>
@@ -11,21 +11,22 @@ export function adView(ad) {
         </ul>
         </div>
     </div>
-    <br>
 </a>`
 }
 
 export function errorView(message) {
-    return `<div class="error">
-        ${message}
-        <button>Cerrar</button>
+    return `
+    <div class="alert alert-dismissible alert-danger">
+    <button type="button" class="btn-close" data-bs-dismiss="alert"></button>
+    <strong>${message}</strong>
     </div>`
 }
 
 export function successView(message) {
-    return `<div class="success">
-        ${message}
-        <button>Cerrar</button>
+    return `
+    <div class="alert alert-dismissible alert-success">
+    <button type="button" class="btn-close" data-bs-dismiss="alert"></button>
+    <strong>${message}</strong>
     </div>`
 }
 
@@ -39,7 +40,7 @@ export function adDetailView(ad) {
     }
     let button = ''
     if (ad.canBeDeleted) {
-        button = '<button class="delete">Borrar</button>'
+        button = '<button class="delete" class="btn btn-danger">Borrar</button>'
     }
     return `
     <div name="post">
